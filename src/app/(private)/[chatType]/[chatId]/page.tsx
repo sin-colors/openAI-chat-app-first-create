b2d0fecba540a9cfb5be374f5fdbcf2a3422b524
sin-colors <shingo6651inagaki2302@i3.gmobb.jp>
2025-10-12 18:06:56 +0900

    ChatTypePage and ChatRoomPage fixed

--- src/app/(private)/[chatType]/[chatId]/page.tsx
+++ src/app/(private)/[chatType]/[chatId]/page.tsx
@@ -1,7 +1,23 @@
+import { redirect } from "next/navigation";
 import React from "react";
 
-function ChatRoomPage() {
-  return <div>ChatRoomPage</div>;
+// ChatTypePage同様、後で修正。
+const allowedRoutes = [
+  "conversation",
+  "image_generation",
+  "text_to_speech",
+  "speech_to_text",
+  "image_analysis",
+];
+
+async function ChatRoomPage({
+  params,
+}: {
+  params: Promise<{ chatType: string }>;
+}) {
+  const { chatType } = await params;
+  if (!allowedRoutes.includes(chatType)) return redirect("/");
+  return <div>ChatRoomPage: {chatType}ページ</div>;
 }
 
 export default ChatRoomPage;
--- src/app/(private)/[chatType]/page.tsx
+++ src/app/(private)/[chatType]/page.tsx
@@ -1,7 +1,23 @@
+import { redirect } from "next/navigation";
 import React from "react";
 
-function ChatTypePage() {
-  return <div>ChatTypePage</div>;
+// 後で修正。必要なくなる予定。
+const allowedRoutes = [
+  "conversation",
+  "image_generation",
+  "text_to_speech",
+  "speech_to_text",
+  "image_analysis",
+];
+
+async function ChatTypePage({
+  params,
+}: {
+  params: Promise<{ chatType: string }>;
+}) {
+  const { chatType } = await params;
+  if (!allowedRoutes.includes(chatType)) return redirect("/");
+  return <div>ChatTypePage: {chatType}ページ</div>;
 }
 
 export default ChatTypePage;
